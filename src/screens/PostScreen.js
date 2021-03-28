5aa048a58913bc9b9c2a72fa49cc2d771b9fd1b4
Serhii B <s_blyshchak@ukr.net>
2021-03-28 15:23:12 +0300

    first commit

---
+++ src/screens/PostScreen.js
@@ -0,0 +1,21 @@
+import React from 'react' 
+import {View, Text, StyleSheet} from 'react-native'
+
+export const PostScreen = ({}) => {
+    return (
+        <View style={styles.center}>
+            <Text>PostScreen</Text>
+        </View>
+    )
+}
+PostScreen.navigationOptions = {
+    headerTitle: 'Пост номер 0'
+}
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
