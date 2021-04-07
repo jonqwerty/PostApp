52970d6ed3fb5e6a1e6a52f74ae64056488320cb
Serhii B <s_blyshchak@ukr.net>
2021-04-07 18:38:22 +0300

    Add PhotoPicker component

---
+++ src/components/PhotoPicker.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react' 
+import * as ImagePicker from 'expo-image-picker';
+import { Button, Image, View, Platform, StyleSheet, Alert} from 'react-native'
+import * as Permissions from 'expo-permissions'
+
+
+async function askForPermissions() {
+    const {status} = await Permissions.askAsync(
+        Permissions.CAMERA,
+        Permissions.CAMERA_ROLL
+    )
+    if (status !== 'granted') {
+        Alert.alert('Помилка', 'Ви не надали права на створення знимки')
+        return false
+    }
+    return true
+}
+
+
+
+export const PhotoPicker = ({}) => {
+    const [image, setImage] = useState(null)
+
+    const takePhoto = async () => {
+        const hasPermissions = await askForPermissions()
+        if (!hasPermissions) {
+            return
+        }
+        const img = await ImagePicker.launchCameraAsync({
+            quality: 0.7,
+            allowsEditing: false,
+            aspect: [16, 9]
+        })
+
+        console.log(img)
+    }
+
+    return (
+            <View style={styles.wrapper}>
+                <Button title='Зробити знимку' onPress={takePhoto}/>
+                {image && <Image style={styles.image} source={{ uri: image }} />}
+            </View>
+        )
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        marginBottom: 10
+    },
+    image: {
+        width: '100%',
+        height: 100,
+        marginTop: 10
+    }
+})
